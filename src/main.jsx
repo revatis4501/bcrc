0d17ae2cbb9148a0666a24b50b44760ffa245917
Kalpesh-Hire <kalpeshhire102050@gmail.com>
2025-11-11 11:16:56 +0530

    navbar bg main change

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import './assets/css/bootstrap.min.css'
 import "animate.css";
 import "./assets/css/style.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
