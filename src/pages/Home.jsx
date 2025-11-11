c2a549d615d734ef95999397cb842210e119478e
unknown <revatis.ek@gmail.com>
2025-11-11 12:55:23 +0530

    Add header carousel component and update dependencies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import HeaderCarousel from '../components/heroCarousel'
 
 const Home = () => {
   return (
     <div>
-      Homeeee
+      {/* Homeeee */}
+      <HeaderCarousel/>
     </div>
   )
 }
