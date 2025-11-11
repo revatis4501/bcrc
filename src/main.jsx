a1e7a46a114bf8090835acdbe10aa4c233b18a9c
Kalpesh Hire <153728545+Kalpesh-Hire@users.noreply.github.com>
2025-11-11 13:47:30 +0530

    changes (#4)

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import "./assets/css/style.css";
 import App from './App.jsx'
 // import "bootstrap/dist/css/bootstrap.min.css";
 import './assets/css/bootstrap.min.css'
 import "animate.css";
-import "./assets/css/style.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
-import HeaderCarousel from '../components/heroCarousel'
+import React from "react";
+import About from "../components/about";
+import Visionmission from "../components/visionmission";
+import Services from "../components/Services";
+import HeaderCarousel from "../components/heroCarousel";
 
 const Home = () => {
   return (
     <div>
       {/* Homeeee */}
       <HeaderCarousel/>
+      <h1>hw</h1>
+      <h1>hw</h1>
+      <h1>hw</h1>
+      <h1>hw</h1>
+      <h1>hw</h1>
+      <h1>hw</h1>
+      <h1>hw</h1>
+      <h1>hw</h1>
+      <h1>hw</h1>
+      <h1>hw</h1>
+      <h1>hw</h1>
+      <h1>hw</h1>
+      <h1>hw</h1>
+      <h1>hw</h1>
+      <h1>hw</h1>
+      <h1>hw</h1>
+      <h1>hw</h1>
+      <About />
+      <Visionmission />
+      <Services/>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
